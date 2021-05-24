e3bd74a3f2c44c6a39881072e5aaed36b60d9626
Daniil Timachov <danyatimachov@gmail.com>
2021-05-24 22:09:18 +0300

    Adding filtering by contents, names and tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,40 @@ def index
     @article = Article.new
   end
 
+  def search  
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else        
+      if params[:search].start_with? 'tag:'
+        # search by tag
+        tag = params[:search][4..-1]
+        @results = Article.all.select { 
+          |article| article.body.downcase.include? '#' + tag.downcase
+        }
+      elsif params[:search].start_with? 'title:'
+        # search by title
+        title = params[:search][6..-1]
+        @results = Article.all.select { 
+          |article| article.title.downcase.include? title.downcase
+        }
+      else
+        # just search in body and title
+        @results = Article.all.select { 
+          |article| article.title.downcase.include? params[:search].downcase or 
+          article.body.downcase.include? params[:search].downcase 
+        }
+      end
+
+      print("\n\n\n\n")
+      
+      for result in @results do
+        print(result)
+      end
+
+      print("\n\n\n\n")
+    end    
+  end
+
   def create
     @article = Article.new(article_params)
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/articles/new", to: "articles#new"
   get "/articles/edit", to: "articles#edit"
   get "/articles/:id", to: "articles#show"
+  get '/search' => 'articles#search', :as => 'search_page'
 
   root 'welcome#index'
   
