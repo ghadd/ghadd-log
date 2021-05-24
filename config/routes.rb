469dc70bba4e6369b4618899c5e3efb27afd31ed
Daniil Timachov <danyatimachov@gmail.com>
2021-05-24 19:05:26 +0300

    Refactoring comments structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'welcome/index'
+  get "index", to: "welcome#index" 
+  get "/home", to: "home#index"
+  get "/articles/new", to: "articles#new"
+  get "/articles/edit", to: "articles#edit"
+  get "/articles/:id", to: "articles#show"
 
   root 'welcome#index'
+  
+  resources :articles do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_095528) do
+ActiveRecord::Schema.define(version: 2021_05_23_111632) do
+
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -23,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_05_23_095528) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "comments", "articles"
 end
